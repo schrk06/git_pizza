8730eb0b927d64afa465fd55e72c5675dbc83bda
creator-craft <crafteur903@gmail.com>
2024-04-11 12:12:13 +0200

    Pizzeria clean

--- Pizza/src/fr/creator_craft/RaPizza/Pizzeria.java
+++ Pizza/src/fr/creator_craft/RaPizza/Pizzeria.java
@@ -17,26 +17,6 @@ public class Pizzeria {
 		catalog = new ArrayList<CataloguedPizza>();
 		used_ingredients = new ArrayList<Ingredient>();
 		order_history = new ArrayList<Order>();
-		
-//		try {
-//			FileReader reader = new FileReader("User.dat");
-//			Scanner s = new Scanner(reader);
-//			
-//			client = new User(s.next(), s.next(), s.next(), s.nextInt(), s.nextFloat(), null);
-//			
-//			s.close();
-//			reader.close();
-//			
-//			reader = new FileReader("Catalog.dat");
-//			s = new Scanner(reader);
-//			
-//			catalog = new CataloguedPizza[s.nextInt()];
-//			
-//			s.close();
-//			reader.close();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
 	}
 	
 	public void loadAll() {
